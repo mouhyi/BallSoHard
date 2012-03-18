c814d469cfd57e0c323aa0368f2da5e2a0a732ec
Ryan Singzon <rcsingzon@gmail.com>
2012-03-18 14:00:05 -0400

    Modified SystemConstants and worked on Localization

--- Localization.java
+++ Localization.java
@@ -1,44 +1,108 @@
-import lejos.nxt.LightSensor;
-
 /**
- * This class is responsible for performing the localization routine
- * USlocalization is executed first to orients the robot parallel to th e Y-axis
- * LSlocalization is then executed to position the robot to (0,0.Pi/2)
- * 
- * @author Mouhyi
- */
+* This class is responsible for performing the localization routine
+* USlocalization is executed first to orients the robot parallel to the Y-axis
+* LSlocalization is then executed to position the robot to (0,0.Pi/2)
+*
+* @author Mouhyi, Joshua David Alfaro
+*/
+
+import lejos.nxt.*;
 
 public class Localization {
 
-	private Odometer odo;
+	// Fields
+	private Coordinates coords;
 	private Robot robot;
 	private LightSensor lsL;
 	private LightSensor lsR;
-	private USPoller usp;
+	private USPoller uspL;
+	private USPoller uspR;
 
+	// Constructor
+	public Localization(Coordinates coords, Robot robot, LightSensor lsL, LightSensor lsR, USPoller uspL, USPoller uspR) {
+		this.coords = coords;
+		this.robot = robot;
+		this.lsL = lsL;
+		this.lsR = lsR;
+		this.uspL = uspL;
+		this.uspR = uspR;
+	}
+	
+	
 	/**
 	 * main Localization routine
-	 * 
+	 *
 	 * @author Mouhyi
 	 */
 	public void doLocalization() {
+		this.doUSLocalization();
 	}
 
 	/**
 	 * US Localization routine: falling edge
-	 * 
-	 * @author Mouhyi
+	 *
+	 * @author Mouhyi, Joshua David Alfaro
 	 */
-	public void doUSLocalization() {
+	private  void doUSLocalization() {
+		// Falling Edge Method
+		double angleA = 0, angleB = 0;
+		
+		int noWall = 0;
+		boolean first_rot = true;
+		boolean second_rot = false;
+		while (first_rot)
+		{
+			robot.rotate(SystemConstants.ROTATION_SPEED);	//rotate counterclockwise
+
+			// Reads no wall
+			if (this.uspL.filter() > 50)
+			{
+				noWall = noWall + 1;
+			}
+
+			// Reads wall after reading no wall (falling edge)
+			if (noWall >  3 && this.uspL.filter() < 30)
+			{
+				first_rot = false;
+				second_rot = true;
+				noWall = 0;
+				angleA = coords.getTheta(); // latch angle
+			}
+		}
+
+		while (second_rot)
+		{
+			robot.rotate(-SystemConstants.ROTATION_SPEED);		// rotate clockwise
+			
+			// Reads no wall
+			if (this.uspR.filter() > 50)
+			{
+				noWall = noWall + 1;
+			}
+
+			// Reads wall after reading no wall (falling edge)
+			if (noWall > 3 && this.uspR.filter() < 30)
+			{
+				second_rot = false;
+				noWall = 0;
+				angleB = coords.getTheta(); // latch angle
+			}
+			
+			
+		}
+		robot.stop();
+		double theta = 45 + Odometer.adjustAngle(angleA - angleB)/2;
+		
+		coords.set(0,0,theta);
 	}
 
 	/**
 	 * Light Localization routine
-	 * 
+	 *
 	 * @author Mouhyi
 	 */
-	public void doLightLocalization() {
+	private void doLightLocalization() {
 
 	}
-
 }
+
--- SystemConstants.java
+++ SystemConstants.java
@@ -5,35 +5,29 @@ import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
 
 /**
- * 
- * Contains all the physical constants of the system
- * All measures must be in cm and angles in DEGREES
- * 
- * @author Mouhyi
- */
+*
+* Contains all the physical constants of the system
+*
+* @author Mouhyi
+*/
 
 public class SystemConstants {
 
 	public static NXTRegulatedMotor leftMotor = Motor.A;
 	public static NXTRegulatedMotor rightMotor = Motor.B;
-	
+
 	public static LightSensor leftLight = new LightSensor(SensorPort.S1);
 	public static LightSensor rightLight = new LightSensor(SensorPort.S4);
-	
-	public static UltrasonicSensor US = new UltrasonicSensor(SensorPort.S2);
-	
-	/**The radii of the robot wheels . */
+
+	public static UltrasonicSensor USL = new UltrasonicSensor(SensorPort.S2);
+	public static UltrasonicSensor USR = new UltrasonicSensor(SensorPort.S3);
+
 	public static final double LEFT_RADIUS=2.57;
 	public static final double RIGHT_RADIUS= 2.56;
-	
-	/** The distance from on wheel to the other  */
 	public static final double WIDTH = 16.1;
 	public static final double LS_WIDTH = 10;
-	
-	private static double FORWARD_SPEED = 13.0; 	 // cm/s
-	private static double ROTATION_SPEED = 40.0;	// Deg/s
-	
-	// 1 foot= 30.48cm
-	private static double TILE = 30.48;
-	
+
+	private static final double FORWARD_SPEED = 13.0; // cm/s
+	public static final double ROTATION_SPEED = 40.0;
+
 }
