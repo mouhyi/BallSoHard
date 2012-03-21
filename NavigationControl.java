02abc18afe181ed043ceb64f6c489d46e41e1f25
Ryan Singzon <rcsingzon@gmail.com>
2012-03-21 01:20:55 -0400

    Modified NavigationControl and USPoller

--- NavigationControl.java
+++ NavigationControl.java
@@ -2,148 +2,155 @@ import lejos.nxt.*;
 import lejos.util.*;
 import lejos.nxt.comm.*;
 
-//The robot is only going to travel on the gridlines
-//If it sees an obstacle, it will find an alternate route 
-
 /*
- * 
+ * Looks for obstacles using the USPoller, then changes direction if it sees something
  * @Author Ryan
- * Change the lab odometer to the actual odometer
- * Use Coordinates instead of coordinates obtained from lab odometer
+ * 
  */
 
 public class NavigationControl implements TimerListener{
 	
 	private final int REFRESH = 50;
 	private Timer ncTimer = new Timer(REFRESH, this);
-	private UltrasonicTimer usLeft, usRight;
-	private LightTimer lsLeft, lsRight;
-	private Robot robot;
-	private Odometer odometer;
+	private USPoller usLeft, usRight;
+	private Odometer odo;
 	private Navigation navigation;
-	private int[] destination;
+	private int leftDistance;
+	private int rightDistance;
+	private Coordinates coords;
+	private int direction;
 	
-	//Initializes the navigation control
-	public NavigationControl(Odometer odometer, UltrasonicTimer usLeft,
-			UltrasonicTimer usRight, LightTimer lsLeft, LightTimer lsRight, Robot robot){
-		
-		this.odometer = odometer;
-		this.usLeft = usLeft;
-		this.usRight = usRight;
-		this.lsLeft = lsLeft;
-		this.lsRight = lsRight;
-		this.robot = robot;
+	/*
+	 * Initializes the navigation control
+	 * @param Navigation, Odometer
+	 * @author Ryan
+	 */
+	public NavigationControl(Navigation navigation, Odometer odo){
+		this.navigation = navigation;
 		this.ncTimer.start();
+		this.odo = odo;
+		this.coords = odo.getCoordinates();
 	}
 	
 	/*
 	 * Continuously polls ultrasonic sensor
-	 * When obstacle is detected, the robot will turn right 90 degrees
+	 * When obstacle is detected, the robot will turn 90 degrees and to avoid the obstacle
+	 * @author Ryan
 	 */
+	
 	public void timedOut(){
-		int leftDistance = usLeft.getDistance();
-		
+		leftDistance = usLeft.getDistance();
+		rightDistance = usRight.getDistance();
+		direction = odo.getDirection();
+				
 		RConsole.println("USLeft: "+String.valueOf(leftDistance));
 		
 		if(usLeft.obstacleDetected()){
 			RConsole.println("Obstacle Detected");
 			usLeft.resetObstacle();
 			changeRoute();
+			this.ncTimer.start();
+			turnRight();
 		}
 		
 		if(usRight.obstacleDetected()){
+			RConsole.println("Obstacle detected");
 			usRight.resetObstacle();
-			
-		}
-		
+			changeRoute();	
+			this.ncTimer.start();
+			turnLeft();
+		}	
 	}
 	
-	
 	/*
-	 * This method also needs to be polling the sensor for more obstacles in the way
-	 * Requires use of light sensor since it is going to turn then move forward until
-	 * it reaches a gridline
-	 * 
+	 * Finds an alternate route to destination
+	 * @author Ryan
 	 */
+	
 	private void changeRoute(){
+		
+		double nearestHorizontalLine;
+		double nearestVerticalLine;
+		
+		int numHorizLines;
+		int numVertLines;
 		
 		//Pause searching for obstacles
 		ncTimer.stop();
 		
-		//Reverse until a gridline crossing is reached
-		RConsole.println("Changing route");
-		//robot.stop();
-		
-		robot.advance(-SystemConstants.FORWARD_SPEED);
-		
-		lsLeft.resetLine();
-		lsRight.resetLine();
-		
-		while(!lsLeft.lineDetected()){}
-		while(!lsRight.lineDetected()){}
-		RConsole.println("Lines detected");
-		
-		//robot.stop();	
-		
-		//Recalibrate heading here
-			
-		//Rotate robot 90 degrees
-		if(odometer.getTheta() == 0){
-			robot.rotate(SystemConstants.ROTATION_SPEED);
-			while(odometer.getTheta()<90){
-			}
-			//robot.stop();
+		//Reverse to closest line, depending on orientation of robot
+		//Facing east
+		if(direction == 0){
+			numVertLines = (int)(coords.getX()/SystemConstants.TILE);
+			nearestVerticalLine = SystemConstants.TILE*numVertLines;
+			navigation.travelTo(nearestVerticalLine, coords.getY());
 		}
 		
-		if(odometer.getTheta() == 90){
-			robot.rotate(SystemConstants.ROTATION_SPEED);
-			while(odometer.getTheta()<180){
-			}
-			//robot.stop();
+		//Facing north
+		else if(direction == 1){
+			numHorizLines = (int)(coords.getY()/SystemConstants.TILE);
+			nearestHorizontalLine = SystemConstants.TILE*numHorizLines;
+			navigation.travelTo(coords.getX(), nearestHorizontalLine);
 		}
 		
-		if(odometer.getTheta() == 180){
-			robot.rotate(SystemConstants.ROTATION_SPEED);
-			while(odometer.getTheta()<270){
-			}
-			//robot.stop();
+		//Facing west
+		else if(direction == 2){
+			numVertLines = 1 + (int)(coords.getX()/SystemConstants.TILE);
+			nearestVerticalLine = SystemConstants.TILE*numVertLines;
+			navigation.travelTo(nearestVerticalLine, coords.getY());
 		}
 		
-		if(odometer.getTheta() == 270){
-			robot.rotate(SystemConstants.ROTATION_SPEED);
-			while(odometer.getTheta()!=0){
-			}
-			//robot.stop();
-		}
-		
+		//Facing south
+		else if(direction == 3){
+			numHorizLines = 1 + (int)(coords.getY()/SystemConstants.TILE);
+			nearestHorizontalLine = SystemConstants.TILE*numHorizLines;
+			navigation.travelTo(coords.getX(), nearestHorizontalLine);
+		}				
+							
 		//Resume looking for obstacles
 		ncTimer.start();
 		
-		//Move forward until a gridline has been crossed
-		robot.advance(SystemConstants.FORWARD_SPEED);
-		try{
-			Thread.sleep(1000);
+	}
+	
+	/*
+	 * Turns left
+	 * @author Ryan
+	 */
+	
+	public void turnLeft(){
+		if(direction == 0){
+			navigation.turnTo(0);
 		}
-		catch(Exception e){}
-		lsLeft.resetLine();
-		lsRight.resetLine();
-		
-		while(!lsLeft.lineDetected()){}
-		while(!lsRight.lineDetected()){}
-		
-		robot.stop();
-			
+		else if(direction == 1){
+			navigation.turnTo(90);
+		}
+		else if(direction == 2){
+			navigation.turnTo(180);
+		}
+		else if(direction == 3){
+			navigation.turnTo(270);
+		}
 	}
 	
-	public void recalculateTrajectory(){
-		
-		destination = navigation.getDestination();
-		navigation.travelTo(destination[0], destination[1]);
-				
+	/*
+	 * Turns right
+	 * @author Ryan
+	 * 
+	 */
+	
+	public void turnRight(){
+		if(direction == 0){
+			navigation.turnTo(270);
+		}
+		else if(direction == 1){
+			navigation.turnTo(180);
+		}
+		else if(direction == 2){
+			navigation.turnTo(90);
+		}
+		else if(direction == 3){
+			navigation.turnTo(0);
+		}
 	}
 	
-	public void adjustHeading(){
-		
-	}
-
 }
--- USPoller.java
+++ USPoller.java
@@ -1,36 +1,92 @@
 import lejos.nxt.UltrasonicSensor;
+import lejos.util.Timer;
 
 /**
  * Wrapper class for the ultrasonic sensors
  * 
- * @author Mouhyi
+ * @author Mouhyi, Ryan
  * @see Printer
  */
 
 public class USPoller implements Runnable {
 
+	private final int TOLERANCE = 30;
+	private int[] usDistances = new int[5];
+	private UltrasonicSensor us;
+	private int median;
+	private boolean obstacleDetected;
+	
 	/**
 	 * Constructor
 	 * 
-	 * @author Mouhyi
+	 * @author Mouhyi, Ryan
 	 */
 	public USPoller (UltrasonicSensor us) {
+		this.us = us;
 	}
 	
 	/**
-     * This method implemets run method of the Runnable interface.
+     * This method implements run method of the Runnable interface.
      *
-     * @author Mouhyi
+     * @author Mouhyi, Ryan
      */
 	public void run() {
+	
+		int i;
+		int maxIndex = usDistances.length-1;
+		int middle = maxIndex/2;
+		
+		//Inserts sensor readings into an array and shifts them for every ping
+		for(i=0; i<maxIndex; i++){
+			usDistances[i]=usDistances[i+1];
+		}
+		usDistances[maxIndex]=us.getDistance();
+		
+		//Sorts data and returns median
+		filter(usDistances, 0);
+		
+		median = usDistances[middle];	
+				
+		if(median < TOLERANCE){
+			obstacleDetected = true;
+		}
+	
 	}
 	
-	/**
-     * This method implemets a filter.
-     *
-     * @author Mouhyi
-     */
-	public int filter() {
-		return 0;
+	/*
+	 * Sorts ultrasonic sensor readings in ascending order
+	 * @author Ryan
+	 */
+	private void filter(int data[], int size) { 
+		int i, j; 
+		for (i = 0; i < size; i++){
+			j = i-1;
+			for (j=j; j>=0 && data[j] > data[j+1]; j--){ 
+				int temp;
+				temp = data[j];
+				data[j] = data[j+1];
+				data[j+1] = temp; 
+			} 
+		}
+	}
+	
+	/*
+	 * Sets a mode for determining whether or an obstacle has been detected
+	 * @author Ryan
+	 */
+	public boolean obstacleDetected(){
+		return obstacleDetected;
+	}
+	
+	/*
+	 * Returns the filtered ultrasonic sensor reading
+	 * @author Ryan
+	 */
+	public int getDistance(){
+		return median;
+	}
+	
+	public void resetObstacle(){
+		obstacleDetected = false;
 	}
 }	
