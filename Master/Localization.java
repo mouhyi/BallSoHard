4dabdc7baa6ba13f658e19274c164f360941d4e5
Anthony Delage <anthony.delage@gmail.com>
2012-04-04 16:43:36 -0400

    Localization cleaned up

--- Master/Localization.java
+++ Master/Localization.java
@@ -6,7 +6,7 @@ package Master;
  * USlocalization is executed first to orients the robot parallel to the Y-axis
  * LSlocalization is then executed to position the robot to (0,0.Pi/2)
  *
- * @author Mouhyi, Joshua David Alfaro
+ * @author Mouhyi, Joshua David Alfaro, Anthony
  */
 
 import lejos.nxt.*;
@@ -46,9 +46,9 @@ public class Localization {
 		// Perform the localization
 		robot.setAcceleration(1500);
 		this.doUSLocalization();
-		this.doLightLocalization3();
+		this.doLightLocalization();
 		robot.rotateAxis(90, 5);
-		this.doLightLocalization3();
+		this.doLightLocalization();
 		robot.rotateAxis(-90, 5);
 		robot.setAcceleration(6000);
 
@@ -146,212 +146,7 @@ public class Localization {
 	}
 
 	/**
-	 * Light Localization routine
-	 * 
-	 * @author Mouhyi, JDAlfaro
-	 */
-	private void doLightLocalization() {
-		// Timer for light sensor depending on speed of robot.
-		robot.rotate(40);
-		LightTimer leftLight = new LightTimer(lsL);
-		LightTimer rightLight = new LightTimer(lsR);
-
-		boolean leftDone = true;
-		int gridLineCountL = 0;
-		boolean linePassedL = false;
-		double[] angleL = { 0, 0, 0, 0 };
-
-		boolean rightDone = true;
-		int gridLineCountR = 0;
-		boolean linePassedR = false;
-		double[] angleR = { 0, 0, 0, 0 };
-
-		double xOffset, yOffset, thetaOffset;
-
-		while (leftDone || rightDone) {
-			robot.rotate(40);
-
-			if (leftLight.lineDetected()) {
-				gridLineCountL = gridLineCountL + 1;
-				linePassedL = true;
-				Sound.setVolume(20);
-				Sound.beep();
-			}
-
-			if (rightLight.lineDetected()) {
-				gridLineCountR = gridLineCountR + 1;
-				linePassedR = true;
-				Sound.setVolume(20);
-				Sound.beep();
-			}
-
-			// latch angles into memory only after a line pass
-			if (linePassedL) {
-				switch (gridLineCountL) {
-				case 1:
-					angleL[gridLineCountL - 1] = odo.getCoordinates()
-							.getTheta();
-					break;
-				case 2:
-					angleL[gridLineCountL - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleL[gridLineCountL - 1] < angleL[gridLineCountL - 2]) {
-						angleL[gridLineCountL - 1] += 360;
-					}
-					break;
-				case 3:
-					angleL[gridLineCountL - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleL[gridLineCountL - 1] < angleL[gridLineCountL - 2]) {
-						angleL[gridLineCountL - 1] += 360;
-					}
-					break;
-				case 4:
-					angleL[gridLineCountL - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleL[gridLineCountL - 1] < angleL[gridLineCountL - 2]) {
-						angleL[gridLineCountL - 1] += 360;
-					}
-					leftDone = false;
-					break;
-				default:
-					break;
-				}
-
-				linePassedL = false;
-				leftLight.resetLine();
-			}
-
-			// latch angles into memory only after a line pass
-			if (linePassedR) {
-				switch (gridLineCountR) {
-				case 1:
-					angleR[gridLineCountR - 1] = odo.getCoordinates()
-							.getTheta();
-					break;
-				case 2:
-					angleR[gridLineCountR - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleR[gridLineCountR - 1] < angleR[gridLineCountR - 2]) {
-						angleR[gridLineCountR - 1] += 360;
-					}
-					break;
-				case 3:
-					angleR[gridLineCountR - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleR[gridLineCountR - 1] < angleR[gridLineCountR - 2]) {
-						angleR[gridLineCountR - 1] += 360;
-					}
-					break;
-				case 4:
-					angleR[gridLineCountR - 1] = odo.getCoordinates()
-							.getTheta();
-					// To avoid wrap around which would currently ruin
-					// computation
-					if (angleR[gridLineCountR - 1] < angleR[gridLineCountR - 2]) {
-						angleR[gridLineCountR - 1] += 360;
-					}
-					rightDone = false;
-					break;
-				default:
-					break;
-				}
-
-				linePassedR = false;
-				rightLight.resetLine();
-			}
-		}
-		robot.stop(); // stop doing moving and leave loop to perform
-						// calculations
-
-		// Stop timers for light timer detection since it is no longer needed.
-		leftLight.stop();
-		rightLight.stop();
-
-		// do trigonometry to compute with reference to real (0,0) and 0 degrees
-		xOffset = ((-1) * SystemConstants.LS_TOCENTRE
-				* Math.cos(Math.toRadians((angleL[2] - angleL[0]) / 2)) + (-1)
-				* SystemConstants.LS_TOCENTRE
-				* Math.cos(Math.toRadians((angleR[2] - angleR[0]) / 2))) / 2;
-		yOffset = ((-1) * SystemConstants.LS_TOCENTRE
-				* Math.cos(Math.toRadians((angleL[3] - angleL[1]) / 2)) + (-1)
-				* SystemConstants.LS_TOCENTRE
-				* Math.cos(Math.toRadians((angleR[3] - angleR[1]) / 2))) / 2;
-
-		// needs work, WORSE CASE = set to 30 degrees since it seems to stop
-		// around there.
-		// y = mx + b
-		double m = 90 / ((angleR[3] + angleL[1]) / 2 - (angleR[2] + angleL[0]) / 2);
-		double b = 180 - (m) * (angleR[2] + angleL[0]) / 2;
-
-		thetaOffset = Odometer.adjustAngle(m * odo.getCoordinates().getTheta()
-				+ b);
-
-		odo.setCoordinates(xOffset, yOffset, thetaOffset, new boolean[] { true,
-				true, true });
-	}
-
-	/**
-	 * Light Localization routine 2 Drive straight towards line and basically
-	 * use odometry correction style correction This is potentially more
-	 * accurate but more time consusming Not sure if needed if odometry
-	 * correction works
-	 * 
-	 * @author Mouhyi, JDAlfaro
-	 */
-	private void doLightLocalization2() // not tested, is another idea, not
-										// finished
-	{
-		double time_left = 0, time_right = 0;
-		boolean leftNotSeen = true, rightNotSeen = true;
-
-		robot.advance(5); // for some reason it needs to be called twice to work
-		LightTimer leftLight = new LightTimer(lsL);
-		LightTimer rightLight = new LightTimer(lsR);
-
-		while (leftNotSeen || rightNotSeen) {
-			if (leftLight.lineDetected()) {
-				time_left = System.currentTimeMillis();
-				leftNotSeen = false;
-				Sound.setVolume(20);
-				Sound.beep();
-			}
-
-			if (rightLight.lineDetected()) {
-				time_right = System.currentTimeMillis();
-				rightNotSeen = false;
-				Sound.setVolume(20);
-				Sound.beep();
-			}
-		}
-
-		double time_difference = Math.abs(time_right - time_left);
-
-		double theta = Math.atan(time_difference
-				* SystemConstants.FORWARD_SPEED / SystemConstants.LS_WIDTH);
-
-		if (time_right > time_left) {
-			robot.rotateAxis(90 + theta, 40);
-		} else {
-			robot.rotateAxis(90 - theta, 40);
-		}
-
-		odo.setCoordinates(SystemConstants.LS_WIDTH * Math.tan(theta) / 2, 0.0,
-				0, new boolean[] { true, false, true });
-	}
-
-	/**
-	 * Light Localization routine 3 This method of localization involves driving
+	 * Light Localization routine This method of localization involves driving
 	 * towards the line, and stopping the left and right wheels when their
 	 * respective light sensors detect a line, essentially lining up the robot
 	 * on a line.
@@ -359,7 +154,7 @@ public class Localization {
 	 * @author Anthony, Josh
 	 */
 
-	private void doLightLocalization3() {
+	private void doLightLocalization() {
 
 		// Initialize two booleans that represent whether or not the right
 		// and left sensors have seen lines
